44c967da63a594587216011729ad3136b8614db8
Jeremiah Akpera <jerryakpera@gmail.com>
2023-07-01 11:56:17 +0100

    This will add SCSS

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <div>
+      <h1>Frontend Mentor Challenges</h1>
+      <h5>
+        Below are an ever growing collection of solutions to Frontend Mentor
+        Challenges
+      </h5>
+    </div>
+
+    <router-view> </router-view>
+    <h4>
+      <!-- Each challenge should have a link to the challenge page, my github code and the actual solution -->
+      <div>List of cards with challenges and links to challenge</div>
+    </h4>
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<script setup></script>
+
+<style scoped lang="scss">
+$link: #d62828;
+$dark: #000814;
+$light: #edf6f9;
+$accent: #003566;
+$primary: #001d3d;
+$secondary: #ffc300;
+
+body {
+  background-color: $dark;
 }
 </style>
